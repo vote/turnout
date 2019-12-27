d19fea7f36dfb5c6bd55b274d81fb2f57781a9d0
Nick Catalano <nickcatal@gmail.com>
2019-12-26 18:54:04 -0600

    Have Travis pass release info to Sentry

--- scripts/travis_ecr_push.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-pip install awscli
-
-eval $(aws ecr get-login --no-include-email --region us-west-2)
-docker build --build-arg TAG_ARG=${TRAVIS_TAG} --build-arg BUILD_ARG=${TRAVIS_BUILD} -t turnout .
-docker tag turnout:latest ${AWS_ACCOUNT_ID}.dkr.ecr.us-west-2.amazonaws.com/turnout:${TRAVIS_TAG}
-docker push ${AWS_ACCOUNT_ID}.dkr.ecr.us-west-2.amazonaws.com/turnout:${TRAVIS_TAG}
