39bbfab6ab754665faae2ca66ce65a3d03efb320
Nick Catalano <nickcatal@gmail.com>
2020-01-02 11:56:41 -0500

    Remove multiprocessing gunicorn. Add this back soon
    This is a slightly premature optimization, and there may be problems
    with postgres pooling the way it's setup now. Let's come back to this
    soon.

--- app/turnout/gunicorn.conf.py
+++ app/turnout/gunicorn.conf.py
@@ -1,11 +1,7 @@
-import multiprocessing
-
 access_log_format = (
     '%(h)s %({x-forwarded-for}i)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'
 )
 timeout = 75
 keepalive = 75
-num_proc = multiprocessing.cpu_count()
-workers = (num_proc * 2) + 1
 accesslog = "-"
 errorlog = "-"
