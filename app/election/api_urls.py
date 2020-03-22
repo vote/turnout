f5a9c21dfa1cf963f75419a31fb59de81a3f86c8
Josh Levinger <josh@levinger.net>
2020-03-22 15:19:22 -0700

    consistent api URL names (#57)

--- app/election/api_urls.py
+++ app/election/api_urls.py
@@ -3,8 +3,8 @@ from rest_framework import routers
 from .api_views import StateFieldsViewSet, StateViewSet
 
 router = routers.SimpleRouter()
-router.register(r"stateinfo", StateViewSet)
-router.register(r"fields", StateFieldsViewSet)
+router.register(r"state", StateViewSet)
+router.register(r"field", StateFieldsViewSet)
 
 app_name = "api_election"
 urlpatterns = router.urls
