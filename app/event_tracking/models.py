1f9e8289f109bcebff06652b710c34b13afd8e4f
Nick Catalano <nickcatal@gmail.com>
2020-05-19 11:51:01 -0400

    Set event_type to be a database index (#141)
    We're going to be doing quite a few aggregate functions using the
    event_type column and it would be very useful to index that column.

--- app/event_tracking/models.py
+++ app/event_tracking/models.py
@@ -7,7 +7,7 @@ from common.utils.models import TimestampModel, UUIDModel
 
 class Event(UUIDModel, TimestampModel):
     action = models.ForeignKey("action.Action", on_delete=models.PROTECT, db_index=True)
-    event_type = TurnoutEnumField(enums.EventType)
+    event_type = TurnoutEnumField(enums.EventType, db_index=True)
 
     class Meta:
         ordering = ["-created_at"]
