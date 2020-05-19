63fab12fe9ec3f7eea0359c7a6c0cbfdc83a3053
Josh Levinger <josh@levinger.net>
2020-05-19 15:37:25 -0700

    make storageitems searchable by admins (#144)

--- app/storage/admin.py
+++ app/storage/admin.py
@@ -6,3 +6,4 @@ from storage import models
 @admin.register(models.StorageItem)
 class StorageItemAdmin(admin.ModelAdmin):
     list_display = ("uuid", "email", "token", "created_at")
+    search_fields = ("email",)
