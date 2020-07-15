f8d9ec36bf31f79cf1927e01d533cc99e54c8b10
Ben Weissmann <ben@voteamerica.com>
2020-07-15 17:03:56 -0400

    Update CO OVR link to go directly to voter reg instead of portal (#273)

--- app/register/custom_ovr_links/co.py
+++ app/register/custom_ovr_links/co.py
@@ -7,6 +7,6 @@ from ..models import Registration
 
 def get_custom_ovr_link_co(registration: Registration) -> Optional[str]:
     if settings.REGISTER_CO_VRD_ENABLED:
-        return f"https://www.sos.state.co.us/voter?vrdid={settings.REGISTER_CO_VRD_ID}&campaign={registration.uuid}"
+        return f"https://www.sos.state.co.us/voter/pages/pub/olvr/verifyNewVoter.xhtml?vrdid={settings.REGISTER_CO_VRD_ID}&campaign={registration.uuid}"
     else:
         return None
