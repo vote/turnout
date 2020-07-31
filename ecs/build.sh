bd95e90f20e164f3a1a716db7185a5c8d5ae33ff
Nick Catalano <nickcatal@gmail.com>
2020-07-31 18:25:58 -0400

    Reduce size of ECS task definitions (#327)
    * Reduce size of ECS task definitions
    
    ECS task definitions max out at 65536 bytes. Ours were at 65937. By
    removing a ton of unnecessary space while we wait to break out task
    definitions we won't run into this problem.
    
    * Add minification to build script

--- ecs/build.sh
+++ ecs/build.sh
@@ -13,11 +13,19 @@ cat <<"EOF" | docker run --rm -i --entrypoint bash -v $PWD/..:/app -w /app/ecs b
 jsonnetfmt -i ./template/*.jsonnet
 jsonnetfmt -i ./template/include/*.libsonnet
 
-jsonnet ./template/ecs_web.jsonnet --ext-str env=dev --ext-code migrations=true > ./generated/dev/turnout_dev_web.task.json
-jsonnet ./template/ecs_web.jsonnet --ext-str env=staging --ext-code migrations=true > ./generated/staging/turnout_staging_web.task.json
-jsonnet ./template/ecs_web.jsonnet --ext-str env=prod --ext-code migrations=false > ./generated/prod/turnout_prod_web.task.json
+jsonnet ./template/ecs_web.jsonnet --ext-str env=dev --ext-code migrations=true > ./generated/dev/turnout_dev_web.task.temp.json
+jsonnet ./template/ecs_web.jsonnet --ext-str env=staging --ext-code migrations=true > ./generated/staging/turnout_staging_web.task.temp.json
+jsonnet ./template/ecs_web.jsonnet --ext-str env=prod --ext-code migrations=false > ./generated/prod/turnout_prod_web.task.temp.json
 
 jsonnet -S ./template/remote_run.jsonnet --ext-str env=prod --ext-code migrations=false > ../scripts/remote_run.sh
 EOF
+
+jq -c . < ./generated/dev/turnout_dev_web.task.temp.json > ./generated/dev/turnout_dev_web.task.json
+jq -c . < ./generated/staging/turnout_staging_web.task.temp.json > ./generated/staging/turnout_staging_web.task.json
+jq -c . < ./generated/prod/turnout_prod_web.task.temp.json > ./generated/prod/turnout_prod_web.task.json
+
+rm ./generated/dev/turnout_dev_web.task.temp.json
+rm ./generated/staging/turnout_staging_web.task.temp.json
+rm ./generated/prod/turnout_prod_web.task.temp.json
 
 chmod 644 $TARGETS
