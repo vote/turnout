vote/turnout

commit 15398642952004b3e48a48f3365cc349de5f64ad

smsbot: fix number PK and foreign keys (#421)
* smsbot: remove unused function

* update models

* migrations

app/smsbot/models.py
@@ -1,4 +1,5 @@
 import logging
+import uuid
 
 from django.conf import settings
 from django.db import models
@@ -22,7 +23,9 @@ else:
 
 
 class SMSMessage(UUIDModel, TimestampModel):
-    phone = PhoneNumberField(null=True, db_index=True)
+    phone = models.ForeignKey(
+        null=True, on_delete=models.deletion.SET_NULL, to="smsbot.Number"
+    )
     direction = TurnoutEnumField(MessageDirectionType)
     message = models.TextField(blank=True, null=True)
     status = TurnoutEnumField(SMSDeliveryStatus, null=True)
@@ -38,8 +41,11 @@ class SMSMessage(UUIDModel, TimestampModel):
         return f"{settings.PRIMARY_ORIGIN}/v1/smsbot/twilio-message-status/{self.uuid}/"
 
 
-class Number(UUIDModel, TimestampModel):
-    phone = PhoneNumberField(null=True, db_index=True)
+class Number(TimestampModel):
+    # deprecated key from old UUIDModel base class
+    uuid = models.UUIDField(default=uuid.uuid4, editable=False)
+
+    phone = PhoneNumberField(primary_key=True)
 
     welcome_time = models.DateTimeField(null=True, blank=True)
     opt_out_time = models.DateTimeField(null=True, blank=True)

app/voter/models.py
@@ -1,7 +1,9 @@
 import datetime
 import logging
+import uuid
 
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
 
 from common.utils.models import TimestampModel, UUIDModel
 
@@ -29,8 +31,11 @@ class Voter(UUIDModel, TimestampModel):
     registration_date = models.DateField(null=True)
     last_registration_refresh = models.DateTimeField(null=True)
 
+    # deprecated:
+    phone_id = models.UUIDField(null=True, editable=False, default=uuid.uuid4)
+
     # our contact info
-    phone = models.ForeignKey("smsbot.Number", null=True, on_delete=models.SET_NULL)
+    phone = PhoneNumberField(null=True)
     email = models.EmailField(max_length=150, null=True)
 
     # PII (pulled from or validated against a data source)

app/smsbot/migrations/0004_dedup_phone.py
@@ -1,0 +1,20 @@
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("smsbot", "0003_auto_20200804_1546"),
+    ]
+
+    operations = [
+        migrations.RunSQL(
+            """
+DELETE FROM smsbot_number
+WHERE ctid NOT IN (
+    SELECT min(ctid)
+    FROM smsbot_number
+    GROUP BY phone);
+            """
+        )
+    ]

app/smsbot/migrations/0005_change_number_pk.py
@@ -1,0 +1,26 @@
+# Generated by Django 2.2.15 on 2020-08-26 21:04
+
+import uuid
+from django.db import migrations, models
+import phonenumber_field.modelfields
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('smsbot', '0004_dedup_phone'),
+        ('voter', '0002_clear_voter_phone'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='number',
+            name='uuid',
+            field=models.UUIDField(default=uuid.uuid4, editable=False),
+        ),
+        migrations.AlterField(
+            model_name='number',
+            name='phone',
+            field=phonenumber_field.modelfields.PhoneNumberField(max_length=128, primary_key=True, region=None, serialize=False),
+        ),
+    ]

app/smsbot/migrations/0006_msg_foreign_key_to_number.py
@@ -1,0 +1,19 @@
+# Generated by Django 2.2.15 on 2020-08-26 21:06
+
+from django.db import migrations, models
+import django.db.models.deletion
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('smsbot', '0005_change_number_pk'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='smsmessage',
+            name='phone',
+            field=models.ForeignKey(null=True, on_delete=django.db.models.deletion.SET_NULL, to='smsbot.Number'),
+        ),
+    ]

app/smsbot/tasks.py
@@ -47,33 +47,6 @@ def send_welcome_sms(number: str, origin: str = None) -> None:
 
 
 @shared_task
-@statsd.timed("turnout.smsbot.tasks.send_reminder_sms")
-def send_reminder_sms(number: str) -> None:
-    from .models import Number
-
-    n = Number.objects.get(phone=number)
-    if not n:
-        # We have no record of this user--do not send them a reminder.
-        return
-
-    if n.opt_out_time:
-        # If the user has (really) opted out, do not send them anything.  Note
-        # that Twilio's advanced opt-in may also prevent us from sending something
-        # even if we tried.  It is also possible they opted-out, and then sent
-        # UNSTOP or JOIN but not YES so that they can receive our message but we
-        # still show them as opted out.  Either way, we should not contact them.
-        return
-
-    msg = (
-        "Reminder: You are subscribed to VoteAmerica Election Alerts. "
-        "Msg & Data rates may apply. 4 msgs/month. "
-        "Reply HELP for help, STOP to cancel."
-    )
-
-    n.send_sms(msg)
-
-
-@shared_task
 def poll_optout() -> None:
     from .optout import poll
 

app/voter/migrations/0002_clear_voter_phone.py
@@ -1,0 +1,49 @@
+# Generated by Django 2.2.15 on 2020-08-26 21:48
+
+import uuid
+from django.db import migrations, models
+
+
+## This is based on https://stackoverflow.com/questions/38311503/django-1-9-drop-foreign-key-in-migration
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('voter', '0001_initial'),
+    ]
+
+    database_operations = [
+        migrations.RunSQL("UPDATE voter_voter SET phone_id=NULL"),
+        # remove DB constraint so that we can make this not a PK on smsbot_number
+        migrations.AlterField(
+            model_name='voter',
+            name='phone',
+            field=models.ForeignKey(
+                null=True,
+                db_constraint=False,
+                on_delete=models.deletion.SET_NULL,
+                to='smsbot.Number'
+            ),
+        ),
+    ]
+
+    state_operations = [
+        # now make it a simple UUID field (not a foreign key)
+        migrations.AlterField(
+            model_name='voter',
+            name='phone',
+            field=models.UUIDField(default=uuid.uuid4, editable=False, null=True),
+        ),
+        migrations.RenameField(
+            model_name='voter',
+            old_name='phone',
+            new_name='phone_id'
+        ),
+    ]
+
+    operations = [
+        migrations.SeparateDatabaseAndState(
+            database_operations=database_operations,
+            state_operations=state_operations
+        )
+    ]

app/voter/migrations/0003_voter_phone.py
@@ -1,0 +1,19 @@
+# Generated by Django 2.2.15 on 2020-08-26 21:48
+
+from django.db import migrations
+import phonenumber_field.modelfields
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('voter', '0002_clear_voter_phone'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='voter',
+            name='phone',
+            field=phonenumber_field.modelfields.PhoneNumberField(max_length=128, null=True, region=None),
+        ),
+    ]

commit aca62abf6eff4823a2fc922e7e3a73cef8e3e726

Make lob webhook callbacks work (#431)
* smsbot: fix texting

Broken by 15398642952004b3e48a48f3365cc349de5f64ad

* lob: fix lob letter status callbacks

* lint

* fix lob callback tests

app/common/models.py
@@ -8,7 +8,6 @@ from django.db import models
 from common.utils.models import TimestampModel, UUIDModel
 from turnout.celery_app import app
 
-
 logger = logging.getLogger("common")
 
 
@@ -36,6 +35,7 @@ class DelayedTask(UUIDModel, TimestampModel):
         try:
             new_id = app.send_task(self.task_name, args=self.args, kwargs=self.kwargs)
         except Exception as e:
-            logger.exception(f"failed to run task {self.task_name} args {self.args} kwargs {self.kwargs}")
+            logger.exception(
+                f"failed to run task {self.task_name} args {self.args} kwargs {self.kwargs}"
+            )
             sentry_sdk.capture_exception(e)
-            pass

app/integration/api_urls.py
@@ -4,5 +4,5 @@ from .api_views import lob_letter_status
 
 app_name = "api_integration"
 urlpatterns = [
-    path("lob-letter-status/<str:pk>/", lob_letter_status),
+    path("lob-letter-status/", lob_letter_status),
 ]

app/integration/api_views.py
@@ -1,16 +1,17 @@
-import json
 import logging
 
 from django.conf import settings
 from django.core.exceptions import ObjectDoesNotExist
-from django.http import HttpResponse
+from django.http import Http404, HttpResponse
 from rest_framework import status
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
 
-from action.models import Action
 from common.analytics import statsd
-from common.enums import EventType
+from common.enums import EventType, ExternalToolType
+from smsbot.models import Number
+
+from .models import Link
 
 logger = logging.getLogger("integration")
 
@@ -20,19 +21,13 @@ def validate_lob_request(func):
     def wrapper(request, *args, **kwargs):
         import hashlib
         import hmac
-        import base64
 
         timestamp = request.headers.get("Lob-Signature-Timestamp", "")
         message = timestamp + "." + request.body.decode("utf-8")
         secret = settings.LOB_LETTER_WEBHOOK_SECRET
-        signature = base64.b64encode(
-            hmac.new(
-                secret.encode("utf-8"),
-                message.encode("utf-8"),
-                digestmod=hashlib.sha256,
-            ).digest()
-        )
-
+        signature = hmac.new(
+            secret.encode("utf-8"), message.encode("utf-8"), digestmod=hashlib.sha256,
+        ).hexdigest()
         if signature != request.headers.get("Lob-Signature"):
             statsd.increment("turnout.integration.bad_lob_signature")
             logger.warning(
@@ -47,7 +42,21 @@ def validate_lob_request(func):
 @api_view(["POST"])
 @permission_classes([AllowAny])
 @validate_lob_request
-def lob_letter_status(request, pk):
+def lob_letter_status(request):
+    letter_id = request.data.get("body", {}).get("id")
+    if not letter_id:
+        logger.warning(f"Got lob webhook without object id: {request.data}")
+        raise Http404
+
+    link = None
+    for link in Link.objects.filter(
+        external_tool=ExternalToolType.LOB, external_id=letter_id
+    ):
+        break
+    if not link:
+        logger.warning(f"Got lob webhook on unknown letter id {letter_id}")
+        raise Http404
+    action = link.action
 
     event_mapping = {
         "letter.mailed": EventType.LOB_MAILED,
@@ -61,16 +70,20 @@ def lob_letter_status(request, pk):
         #        "letter.returned_to_sender": "Unfortunately, your absentee ballot application has been returned to sender.",
     }
 
-    try:
-        action = Action.objects.get(pk=pk)
-        info = json.loads(request.body)
-        etype = info.get("event_type", {}).get("id")
-        if etype in event_mapping:
-            action.track_event(event_mapping[etype])
-        if etype in event_sms:
-            number = event.get_sms_number()
-            if number:
+    etype = request.data.get("event_type", {}).get("id")
+
+    item = action.get_source_item()
+    logger.info(f"Received lob status update on {item} of {etype}")
+
+    if etype in event_mapping:
+        action.track_event(event_mapping[etype])
+
+    if etype in event_sms:
+        if item.phone:
+            try:
+                number = Number.objects.get(phone=item.phone)
                 number.send_sms(event_sms[etype])
-    except ObjectDoesNotExist:
-        return HttpResponse("Event id not found", status=status.HTTP_404_NOT_FOUND)
+            except ObjectDoesNotExist:
+                pass
+
     return HttpResponse()

app/smsbot/models.py
@@ -35,7 +35,7 @@ class SMSMessage(UUIDModel, TimestampModel):
         ordering = ["-created_at"]
 
     def __str__(self):
-        return f"SMSMessage - {self.phone} {self.direction}"
+        return f"SMSMessage - {self.phone_id} {self.direction}"
 
     def delivery_status_webhook(self):
         return f"{settings.PRIMARY_ORIGIN}/v1/smsbot/twilio-message-status/{self.uuid}/"
@@ -65,7 +65,7 @@ class Number(TimestampModel):
             return
         with tracer.trace("smsbot.number.send_sms", service="twilio"):
             msg = SMSMessage.objects.create(
-                phone=str(self.phone), direction=MessageDirectionType.OUT, message=text,
+                phone=self, direction=MessageDirectionType.OUT, message=text,
             )
             r = twilio_client.messages.create(
                 to=str(self.phone),

app/integration/tests/test_lob_api_views.py
@@ -7,17 +7,19 @@ from rest_framework.test import APIClient
 
 from common.enums import (
     EventType,
+    ExternalToolType,
     StateFieldFormats,
     SubmissionType,
     TurnoutActionStatus,
 )
 from event_tracking.models import Event
+from integration.models import Link
 
-LOB_LETTER_STATUS_API_ENDPOINT = "/v1/integration/lob-letter-status/{uuid}/"
+LOB_LETTER_STATUS_API_ENDPOINT = "/v1/integration/lob-letter-status/"
 
 LOB_LETTER_STATUS_BODY = {
     "id": "evt_d95ff8ffd2b5cfb4",
-    "body": {"id": "psc_d2d10a2e9cba991c", "blah": "blah",},
+    "body": {"id": "ltr_d2d10a2e9cba991c", "blah": "blah",},
     "event_type": {
         "id": "letter.mailed",
         "enabled_for_test": True,
@@ -34,25 +36,27 @@ LOB_LETTER_STATUS_BODY = {
 def test_lob_letter_status():
     import hashlib
     import hmac
-    import base64
 
     settings.LOB_LETTER_WEBHOOK_SECRET = "foo"
 
     ballot_request = baker.make_recipe("absentee.ballot_request")
+    link = Link.objects.create(
+        action=ballot_request.action,
+        external_tool=ExternalToolType.LOB,
+        external_id=LOB_LETTER_STATUS_BODY["body"]["id"],
+    )
 
     timestamp = "last tuesday"
     body = json.dumps(LOB_LETTER_STATUS_BODY)
     message = timestamp + "." + body
     secret = settings.LOB_LETTER_WEBHOOK_SECRET
-    signature = base64.b64encode(
-        hmac.new(
-            secret.encode("utf-8"), message.encode("utf-8"), digestmod=hashlib.sha256
-        ).digest()
-    )
+    signature = hmac.new(
+        secret.encode("utf-8"), message.encode("utf-8"), digestmod=hashlib.sha256
+    ).hexdigest()
 
     client = APIClient()
     response = client.post(
-        LOB_LETTER_STATUS_API_ENDPOINT.format(uuid=ballot_request.action.uuid),
+        LOB_LETTER_STATUS_API_ENDPOINT,
         body,
         content_type="application/json",
         **{"HTTP_LOB_SIGNATURE_TIMESTAMP": timestamp, "HTTP_LOB_SIGNATURE": signature,},
@@ -66,18 +70,18 @@ def test_lob_letter_status():
 # Test lob letter status callbacks
 @pytest.mark.django_db
 def test_lob_letter_status_bad_signature():
-    import base64
+    pass
 
     settings.LOB_LETTER_WEBHOOK_SECRET = "foo"
 
-    ballot_request = baker.make_recipe("absentee.ballot_request")
+    baker.make_recipe("absentee.ballot_request")
 
     timestamp = "today"
-    signature = base64.b64encode("not a valid hash value".encode("utf-8"))
+    signature = "abc123deadbeef"
 
     client = APIClient()
     response = client.post(
-        LOB_LETTER_STATUS_API_ENDPOINT.format(uuid=ballot_request.action.uuid),
+        LOB_LETTER_STATUS_API_ENDPOINT,
         json=LOB_LETTER_STATUS_BODY,
         **{
             "HTTP_X_LOB_SIGNATURE_TIMESTAMP": timestamp,