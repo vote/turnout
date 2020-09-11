829069fd28f28f8ee91e6590cd7b0ade0cc9d0b4
Sage Weil <sage@newdream.net>
2020-09-11 18:35:06 -0500

    password reset (#500)
    * functional password reset (with ugly default templates)
    
    * make it pretty
    
    * move under /manage, adjust urls a bit

--- app/manage/urls.py
+++ app/manage/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.contrib.auth import views as auth_views
 from django.urls import include, path
 
 from manage import views
@@ -6,6 +8,39 @@ app_name = "manage"
 urlpatterns = [
     path("login/", views.LoginView.as_view(), name="login"),
     path("logout/", views.LogoutView.as_view(), name="logout"),
+    path(
+        "password_reset/",
+        auth_views.PasswordResetView.as_view(
+            template_name="management/auth/password_reset_form.html",
+            success_url="done",
+            from_email=f"VoteAmerica <{settings.DEFAULT_EMAIL_FROM}>",
+            subject_template_name="management/auth/password_reset_subject.txt",
+            email_template_name="management/auth/password_reset_email.html",
+        ),
+        name="password_reset",
+    ),
+    path(
+        "password_reset/done/",
+        auth_views.PasswordResetDoneView.as_view(
+            template_name="management/auth/password_reset_done.html",
+        ),
+        name="password_reset_done",
+    ),
+    path(
+        "password_reset_confirm/<uidb64>/<token>/",
+        auth_views.PasswordResetConfirmView.as_view(
+            template_name="management/auth/password_reset_confirm.html",
+            success_url="/manage/password_reset_complete/",
+        ),
+        name="password_reset_confirm",
+    ),
+    path(
+        "password_reset_complete/",
+        auth_views.PasswordResetCompleteView.as_view(
+            template_name="management/auth/password_reset_complete.html",
+        ),
+        name="password_reset_complete",
+    ),
     path("profile/", include("accounts.manage_profile_urls")),
     path("election/", include("election.manage_urls")),
     path("absentee_dashboard/", include("absentee.dashboard.manage_urls")),
