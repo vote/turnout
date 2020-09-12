vote/turnout

commit dedaab02f694960f047f35e172e93e0c10aadd40

misc fixes of errors seen in sentry (#495)
* official/usvf: retry on 504 (and others)

Address error sentry

* verifier: use session; add retries for alloy and targetsmart

* storage: track links for lob PDF downloads

We stopped linking to these, but there are ~300 emails in wild that
we do not want to result in an exception.

* absentee: behave when ballotrequest has not region

app/absentee/notification.py
@@ -50,13 +50,6 @@ logger = logging.getLogger("absentee")
 def compile_email(
     ballot_request: BallotRequest, template: str, preheader: bool = True
 ) -> str:
-    contact_info = get_absentee_contact_info(ballot_request.region.external_id)
-    mailing_address = (
-        contact_info.full_address
-        if contact_info
-        else "We were unable to find your local election official mailing address"
-    )
-
     query_params = ballot_request.get_query_params()
 
     recipient = {
@@ -69,11 +62,17 @@ def compile_email(
         "subscriber": ballot_request.subscriber,
         "recipient": recipient,
         "state_info": ballot_request.state.data,
-        "mailing_address": mailing_address,
         "query_params": query_params,
     }
     if ballot_request.result_item:
         context["download_url"] = ballot_request.result_item.download_url
+    if ballot_request.region:
+        contact_info = get_absentee_contact_info(ballot_request.region.external_id)
+        context["mailing_address"] = (
+            contact_info.full_address
+            if contact_info
+            else "We were unable to find your local election official mailing address"
+        )
 
     if contact_info.email or contact_info.phone:
         contact_info_lines = []

app/official/usvf.py
@@ -25,8 +25,21 @@ logger = logging.getLogger("official")
 
 
 def authenticated_session() -> requests.Session:
+    from requests.adapters import HTTPAdapter
+    from requests.packages.urllib3.util.retry import Retry
+
     session = requests.Session()
     session.headers["Authorization"] = f"OAuth {settings.USVOTEFOUNDATION_KEY}"
+    session.mount(
+        "https://",
+        HTTPAdapter(
+            max_retries=Retry(
+                total=5,
+                status_forcelist=[500, 502, 503, 504],
+                method_whitelist=["HEAD", "GET"],
+            )
+        ),
+    )
     return session
 
 

app/storage/models.py
@@ -67,7 +67,12 @@ class StorageItem(SubscriberModel, UUIDModel, TimestampModel):
 
     def track_download(self):
         if self.app == enums.FileType.REGISTRATION_FORM:
-            registration = Registration.objects.get(result_item=self)
+            try:
+                registration = Registration.objects.get(result_item=self)
+            except Registration.ObjectDoesNotExist:
+                # we briefly linked to lob PDFs to download, but
+                # stopped.  some links are still in the wild.
+                registration = Registration.objects.get(result_item_mail=self)
             registration.action.track_event(enums.EventType.DOWNLOAD)
         if self.app == enums.FileType.ABSENTEE_REQUEST_FORM:
             ballot_request = BallotRequest.objects.get(result_item=self)

app/verifier/alloy.py
@@ -49,6 +49,27 @@ ALLOY_STATES_ENABLED = ALLOY_STATES_MONTHLY + ALLOY_STATES_WEEKLY
 # update frequencies per https://alloy.us/verify/details/
 
 
+def get_session():
+    from requests.adapters import HTTPAdapter
+    from requests.packages.urllib3.util.retry import Retry
+
+    session = requests.Session()
+    session.auth = requests.auth.HTTPBasicAuth(
+        settings.ALLOY_KEY, settings.ALLOY_SECRET
+    )
+    session.mount(
+        "https://",
+        HTTPAdapter(
+            max_retries=Retry(
+                total=1,
+                status_forcelist=[500, 502, 503, 504],
+                method_whitelist=["HEAD", "GET"],
+            )
+        ),
+    )
+    return session
+
+
 def query_alloy(serializer_data):
     address2 = serializer_data.get("address2", "")
     if address2 is None:
@@ -66,12 +87,9 @@ def query_alloy(serializer_data):
         "birth_date": serializer_data["date_of_birth"].strftime("%Y-%m-%d"),
     }
 
+    session = get_session()
     with tracer.trace("alloy.verify", service="alloyapi"):
-        response = requests.get(
-            ALLOY_ENDPOINT,
-            params=query,
-            auth=requests.auth.HTTPBasicAuth(settings.ALLOY_KEY, settings.ALLOY_SECRET),
-        )
+        response = session.get(ALLOY_ENDPOINT, params=query,)
 
     if response.status_code != 200:
         return {"error": f"HTTP error {response.status_code}: {response.text}"}
@@ -82,13 +100,9 @@ def query_alloy(serializer_data):
 def get_alloy_state_freshness(state):
     freshness = cache.get("alloy_freshness", None)
     if not freshness:
+        session = get_session()
         with tracer.trace("alloy.freshness", service="alloyapi"):
-            response = requests.get(
-                ALLOY_FRESHNESS_ENDPOINT,
-                auth=requests.auth.HTTPBasicAuth(
-                    settings.ALLOY_KEY, settings.ALLOY_SECRET
-                ),
-            )
+            response = session.get(ALLOY_FRESHNESS_ENDPOINT,)
         freshness = response.json().get("data", {}).get("data_freshness", {})
         if freshness:
             cache.set("alloy_freshness", freshness)

app/verifier/targetsmart.py
@@ -15,6 +15,25 @@ logger = logging.getLogger("verifier")
 TARGETSMART_ENDPOINT = "https://api.targetsmart.com/voter/voter-registration-check"
 
 
+def get_session():
+    from requests.adapters import HTTPAdapter
+    from requests.packages.urllib3.util.retry import Retry
+
+    session = requests.Session()
+    session.headers["x-api-key"] = settings.TARGETSMART_KEY
+    session.mount(
+        "https://",
+        HTTPAdapter(
+            max_retries=Retry(
+                total=1,
+                status_forcelist=[500, 502, 503, 504],
+                method_whitelist=["HEAD", "GET"],
+            )
+        ),
+    )
+    return session
+
+
 def query_targetsmart(serializer_data):
     # TS is pretty conservative, so we'll only pass in the bare minimum needed for a
     # match.
@@ -35,12 +54,9 @@ def query_targetsmart(serializer_data):
         "dob": serializer_data["date_of_birth"].strftime("%Y*"),
     }
 
+    session = get_session()
     with tracer.trace("ts.registration_check", service="targetsmartapi"):
-        response = requests.get(
-            TARGETSMART_ENDPOINT,
-            params=query,
-            headers={"x-api-key": settings.TARGETSMART_KEY},
-        )
+        response = session.get(TARGETSMART_ENDPOINT, params=query,)
 
     if response.status_code != 200:
         return {"error": f"HTTP error {response.status_code}: {response.text}"}

commit 4429abb2a094da1d52b37f6756d39b335f4e4c17

absetnee: fix contact_info scope (#503)
Broken by dedaab02f694960f047f35e172e93e0c10aadd40

app/absentee/notification.py
@@ -50,6 +50,7 @@ logger = logging.getLogger("absentee")
 def compile_email(
     ballot_request: BallotRequest, template: str, preheader: bool = True
 ) -> str:
+    contact_info = get_absentee_contact_info(ballot_request.region.external_id)
     query_params = ballot_request.get_query_params()
 
     recipient = {
@@ -67,7 +68,6 @@ def compile_email(
     if ballot_request.result_item:
         context["download_url"] = ballot_request.result_item.download_url
     if ballot_request.region:
-        contact_info = get_absentee_contact_info(ballot_request.region.external_id)
         context["mailing_address"] = (
             contact_info.full_address
             if contact_info