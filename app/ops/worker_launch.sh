d41e3a7f4a67246368dc1e739eca2bfea12f64d0
Nick Catalano <nickcatal@gmail.com>
2020-09-14 09:26:45 -0400

    Reduce crosstalk between workers (#508)
    Celery workers keep trying to send heartbeat responses to each other on
    a queue that doesn't exist. We don't need our workers to coordinate with
    one another, or to verify the health of each other, so we can disable
    that functionality. Will also reduce the amount of unnecessary noise
    going across redis.

--- app/ops/worker_launch.sh
+++ app/ops/worker_launch.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ddtrace-run celery -A turnout.celery_app worker -Q ${1} --without-heartbeat
+ddtrace-run celery -A turnout.celery_app worker -Q ${1} --without-heartbeat --without-mingle --without-gossip
