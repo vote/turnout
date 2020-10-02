a455c2ddf432d1955bada3a27f291e68d7a92997
Sage Weil <sage@newdream.net>
2020-10-02 13:47:38 -0500

    apm: filter out single-span common.tasks.process_token traces (#597)
    * apm: filter out single-span common.tasks.process_token traces
    
    * fix name
    
    * some extra celery stuff
    
    * asdf
    
    * log all the things
    
    * Attach tracer and filter to celery app
    
    * Bump up error level
    
    * Also filter out process token
    
    * Add environment key
    
    * Add example env
    
    * Only enable worker patching if there is a datadog key
    
    * Lint
    
    Co-authored-by: Nick Catalano <nickcatal@gmail.com>

--- app/common/apm.py
+++ app/common/apm.py
@@ -15,7 +15,24 @@ class TurnoutTracer(Tracer):
         return super().write(spans)
 
 
+class FilterResource(object):
+    def __init__(self, resource):
+        self.resource = resource
+
+    def process_trace(self, trace):
+        logger.debug(trace[0].resource)
+        if trace[0].resource == self.resource:
+            return None
+        return trace
+
+
 tracer = TurnoutTracer()
 tracer.configure(
-    settings={"FILTERS": [ddtrace.filters.FilterRequestsOnUrl(r".+/-/health/$"),],}
+    settings={
+        "FILTERS": [
+            FilterResource("common.tasks.enqueue_tokens"),
+            FilterResource("common.tasks.process_token"),
+            ddtrace.filters.FilterRequestsOnUrl(r".+/-/health/$"),
+        ],
+    }
 )
--- app/turnout/celery_app.py
+++ app/turnout/celery_app.py
@@ -1,16 +1,24 @@
 import os
 import time
 
-from celery import Celery
-from ddtrace import patch, patch_all
+import celery
+import ddtrace
+import psycopg2
+import redis
+
+from common.apm import tracer
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "turnout.settings")
 
-if os.environ.get("DEBUG") != "True":
-    patch_all()
-    patch(celery=True)
+if os.environ.get("DATADOG_API_KEY"):
+    ddtrace.patch_all()
 
-app = Celery("turnout")
+app = celery.Celery("turnout")
+
+if os.environ.get("DATADOG_API_KEY"):
+    ddtrace.Pin.override(psycopg2, tracer=tracer)
+    ddtrace.Pin.override(redis, tracer=tracer)
+    ddtrace.Pin.override(app, tracer=tracer)
 
 app.config_from_object("django.conf:settings", namespace="CELERY")
 
