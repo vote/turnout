d6d9dd3e41d2a5336d3f4f50a77c9643c1bb11ba
Sage Weil <sage@newdream.net>
2020-10-03 12:11:31 -0500

    movers: disable blank forms for NC and MN (#606)

--- app/integration/movers.py
+++ app/integration/movers.py
@@ -198,14 +198,12 @@ def send_blank_register_forms(offset=0, limit=None, state=None) -> None:
 
     # post-experiment, moving forward
     states = [
-        "NC",  # postmarked by 2020-10-09
         "KS",  # postmarked by 2020-10-13
         "NE",  # postmarked by 2020-10-16
         "AL",  # postmarked by 2020-10-19
         "MI",  # postmarked by 2020-10-19
         "CO",  # postmarked by 2020-10-26
         "MT",  # postmarked by 2020-10-26
-        "MN",  # received by 2020-10-13
         "PA",  # received by 2020-10-19
         "ME",  # received by 2020-10-19
         "UT",  # received by 2020-10-23
